d80ae774e29da503d37c35e90c5a049f52c8adcf
Andrei Aleksanian <andrei.aleksanian@student.manchester.ac.uk>
2021-02-08 10:32:45 +0000

    Render all main UI elemnents

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import classes from './App.module.css'
+import News from './components/News';
+
 const App = () => {
+  const Header = () => (
+    <div role="header" className={classes.Header}>
+      <h1 role="h1">News</h1>
+      <select role="select" defaultValue="">
+        <option value="">Filter By Source</option>
+      </select>
+    </div>
+  )
+
+  const Footer = () => (
+    <div role="footer" className={classes.Footer}>
+      <button role="showMore" type="button">Show More</button>
+    </div>
+  )
+
   return (
-    <div>
-      
+    <div role="newsWidget" className={classes.Widget}>
+      <Header />
+      <News />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/News/News.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import classes from "./News.module.css"
+
+const News = () => {
+    return (
+        <div role="newsSpace" className={classes.News}>
+            
+        </div>
+    )
+}
+
+export default News;
