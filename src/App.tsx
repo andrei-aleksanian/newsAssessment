c0fb636e7c5ba3cdd5a6fae00703fedd24547704
Andrei Aleksanian <andrei.aleksanian@student.manchester.ac.uk>
2021-02-08 12:17:36 +0000

    Set Sources to main page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import classes from './App.module.css'
 import News from './components/News';
 import {ArticleProps} from './components/News/components/Article'
 
+export interface Source {
+  name: string,
+  id: string
+}
+
 export const getArticles = async (page: number) => {
     const url = 'http://newsapi.org/v2/top-headlines?' +
           'country=us&' +
@@ -18,19 +23,32 @@ export const getArticles = async (page: number) => {
     return data.articles;
 }
 
+export const getSources = (articles: ArticleProps[]) => {
+  let sources = articles.map(a => a.source) as Source[];
+
+  // I was trying to get unique objects here as my last step but didn't figure out how to do that yet
+ 
+  return sources;
+}
+
 const App = () => {
 
   const [page, setPage] = useState(1);
   const [articles, setArticles] = useState([] as ArticleProps[])
 
-  const Header = () => (
-    <div role="header" className={classes.Header}>
+  const Header = () => {
+    const sources = getSources(articles);
+
+    return (
+      <div role="header" className={classes.Header}>
       <h1 role="h1">News</h1>
       <select role="select" defaultValue="">
         <option value="">Filter By Source</option>
+        {sources.map((s:any) => <option key={s.id} value={s.id}>{s.name}</option>)}
       </select>
     </div>
-  )
+    )
+  }
 
   const Footer = () => (
     <div role="footer" className={classes.Footer}>
